97fcbe44b6b5e40601d7ff3d37aaef91796001ea
Sharleen - CNO <sharleen@fastmail.com>
2020-08-13 13:42:46 -0400

    add loadd_packages function

--- R/helpers.R
+++ R/helpers.R
@@ -12,3 +12,16 @@ contains_rmarkdown <- function(filepath) {
 `%||%` <- function(x, y) {
   if (is.null(x)) y else x
 }
+
+
+#' Load packages in the packages.R file
+#'
+#' @return All packages
+#' @export
+#'
+loadd_packages <- function(){
+
+  ## Load your packages, e.g. library(drake).
+  source("./packages.R")
+
+}
