8f5c9d1a7b5604406a30f9cedf4383c7d31b4434
Sharleen - CNO <sharleen@fastmail.com>
2020-08-13 13:52:40 -0400

    customize template

--- inst/templates/packages.R
+++ inst/templates/packages.R
@@ -1,4 +1,11 @@
-## library() calls go here
 library(conflicted)
 library(dotenv)
 library(drake)
+library(renv)
+conflicted::conflict_prefer("filter", "dplyr", quiet = TRUE)
+
+## library() calls go here
+library(dplyr)
+library(rcno)
+library(here)
+library(janitor)
